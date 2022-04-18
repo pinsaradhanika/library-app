b73e0d8d01a78626b77f65fe64f37d1fe0ca46cb
Dilini Jayasiri <dilininadeesha2018@gmail.com>
2022-04-18 13:55:53 +0530

    delete unnecessary files and install saas,react-bootstrap,Raleway google font and develop welcome screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './assets/styles/main.scss';
+import Library from './components/view/Library';
+// import WelcomeScreen from './components/WelcomeScreen';
+
+//Library.tsx inherit to app.tsx
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Library/>
   );
 }
 
